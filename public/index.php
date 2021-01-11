7d57027db0d5e6be3bce4092fa0994b302a24b42
langdonglei <langdonglei@sina.com>
2021-01-11 21:04:09 +0800

    favorite

--- public/index.php
+++ public/index.php
@@ -1,2 +1,28 @@
 <?php
-echo 333;
+$image            = imagecreatetruecolor(1080, 100);
+$color_background = imagecolorallocatealpha($image, 0, 51, 51, 0);
+$color_number     = imagecolorallocatealpha($image, 255, 255, 255, 0);
+$color_text       = imagecolorallocatealpha($image, 255, 255, 255, 0);
+imagefill($image, 0, 0, $color_background);
+
+$number_favorite  = 0;
+$number_following = 0;
+$number_follower  = 0;
+
+$y = 58;
+$x = 10;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', $number_favorite);
+$x += 100;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', '获赞');
+$x += 100;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', $number_following);
+$x += 100;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', '关注');
+$x += 100;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', $number_follower);
+$x += 100;
+imagettftext($image, 25, 0, $x, $y, $color_text, '../font/STHeiti.ttc', '粉丝');
+
+
+header('content-type:image/jpeg');
+imagejpeg($image);
